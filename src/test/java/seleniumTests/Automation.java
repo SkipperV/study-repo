4229805a5ac65f268ed23dbfca311aebe0551b6d
SkipperV <vlad.tymchuk@gmail.com>
2023-04-11 18:27:39 +0300

    TextBox Test

--- src/test/java/seleniumTests/Automation.java
+++ src/test/java/seleniumTests/Automation.java
@@ -4,22 +4,48 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static java.lang.Thread.sleep;
+
 public class Automation {
-    @Test
-    public void checkLoginAmazon() {
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("https://www.amazon.com/ref=nav_bb_logo");
-        driver.manage().window().maximize();
-
-        driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("Iphone 13 case");
-        driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
-
-        Assert.assertTrue(driver.getCurrentUrl().contains("Iphone+13+case"));
-        Assert.assertTrue(driver.findElement(By.xpath("//span[contains(text(), 'Iphone 13')]")).isDisplayed());
-
+    WebDriver driver;
+    @BeforeMethod
+    public void beforeTest() {
+        driver = new ChromeDriver();
+    }
+    @AfterMethod
+    public void closeWindow() {
         driver.quit();
     }
+    @Test
+    public void checkSubmit() {
+        driver.get("https://demoqa.com/text-box");
+
+        driver.findElement(By.xpath("//input[@id='userName']")).sendKeys("Vlad Tymchuk");
+        driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys("test@gmail.com");
+        driver.findElement(By.xpath("//textarea[@id='currentAddress']")).sendKeys("abcd");
+        driver.findElement(By.xpath("//textarea[@id='permanentAddress']")).sendKeys("efgh");
+        driver.findElement(By.xpath("//button[@id='submit']")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//p[@id='name']")).isEnabled());
+
+//        driver.quit();
+    }
+
+//    @Test
+//    public void checkCheckBox() throws InterruptedException {
+//        driver.get("https://demoqa.com/checkbox");
+//
+//        driver.findElement(By.xpath("//label[@for='tree-node-home']/preceding-sibling::button")).click();
+////        driver.findElement(By.xpath("//label[@for='tree-node-desktop']//svg[contains(@class, 'rct-icon-check')]")).click();
+//        driver.findElement(By.xpath("//label[@for='tree-node-desktop']//child::span")).click();
+//        driver.findElement(By.xpath("//label[@for='tree-node-desktop']/preceding-sibling::button")).click();
+//
+//        sleep(2000);
+//        Assert.assertTrue(driver.findElement(By.xpath("//label[@for='tree-node-notes']")).is());
+////        driver.quit();
+//    }
 }
